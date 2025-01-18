eeaac44a17e09711c84f468227872395e0928b53
andrey <andrey@sublimestudio.es>
2025-01-18 12:36:34 +0100

    --wip-- [skip ci]

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,18 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDc9CRE2tdI7996jtUQUWEv8-nhS1T4rEA",
+  authDomain: "aqui-estoy-c47ba.firebaseapp.com",
+  projectId: "aqui-estoy-c47ba",
+  storageBucket: "aqui-estoy-c47ba.firebasestorage.app",
+  messagingSenderId: "239859023022",
+  appId: "1:239859023022:web:a76b97453f644b7379413f",
+  measurementId: "G-8GVGD36RMQ",
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const firebaseApp = initializeApp(firebaseConfig);
+
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,44 +1,90 @@
 <script>
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../../firebaseConfig';
+import { onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../../firebaseConfig';
+import { isAuthenticated } from '../dataholder';
+import { useStore } from '@nanostores/vue';
+import { addDoc, collection } from 'firebase/firestore';
 
 export default {
-    data () {
+    data() {
         return {
-            email : "",
-            password : ""
+            email: "",
+            password: ""
         }
-    }, 
-    methods : {
+    },
+    methods: {
         async login() {
             try {
-                const userCredential = await signInWithEmailAndPassword(auth, this.email, this.password);
-                console.log(userCredential.user);
+                console.log(this.email, this.password)
+                const userCredential = await signInWithEmailAndPassword(auth, this.email, this.password)
+                console.log(userCredential);
+
+                isAuthenticated.set(true);
 
             } catch (Error) {
                 console.log(Error);
             }
         }
+        ,
+        addInstructions() {
+            const instructions = [
+                {
+                    "title": "Crea un anuncio en minutos",
+                    "description": "Cuéntanos sobre tu mascota, agrega fotos y detalles. ¡Así todos pueden ayudarte!"
+                },
+                {
+                    "title": "Hazte parte de la búsqueda:",
+                    "description": "Echa un vistazo a los anuncios de otros usuarios. Si ves a una mascota que te suena familiar, ¡avísales!"
+                },
+                {
+                    "title": "Recibe alertas y actualizaciónes instantáneas",
+                    "description": "Mantente informado sobre cualquier nueva pista o avistamiento, ¡sin perder ningún segundo!"
+                }
+            ]
+
+            instructions.forEach(instruction => {
+                addDoc(collection(db, 'instructions'), instruction).then(docRef => {
+                    console.log("Document written with ID: ", docRef.id);
+                }).catch((error) => {
+                    console.error("Error adding document: ", error);
+                });
+            })
+        }
+    },
+    mounted() {
+        onAuthStateChanged(auth, (user) => {
+            console.log(user)
+            if (user) {
+                console.log("Usuario autenticado", user.uid)
+            } else {
+                console.log("Usuario no autenticado")
+            }
+
+
+
+            isAuthenticated.set(true);
+        })
     }
+
 }
 </script>
 
 <template>
     <div>
-        <label class="form-label"
-          >Nombre de usuario o dirección de correo electrónico</label
-        >
+        <label class="form-label">Nombre de usuario o dirección de correo electrónico</label>
         <input v-model="email" type="email" required class="input-field" />
         <label class="form-label password-label">
-          Contraseña
-          <button type="button" class="password-toggle">
-          </button>
+            Contraseña
+            <button type="button" class="password-toggle">
+            </button>
         </label>
         <input v-model="password" type="password" required class="input-field" />
         <div class="form-links password">
-          <a href="#">¿Olvidaste tu contraseña?</a>
+            <a href="#">¿Olvidaste tu contraseña?</a>
         </div>
-        <button type="submit" class="login-button">Acceder</button>
+        <button @click="login" type="button" class="login-button">Acceder</button>
+
+        <button @click="addInstructions">Add instructions</button>
 
     </div>
 
